c568388f371fc8a4692221248e5a265f4ed975cb
Gustavo Garcia <garcitavo@live.com>
2018-01-13 19:09:08 -0500

    add the conn.php

---
+++ models/conn.php
@@ -0,0 +1,15 @@
+
+<?php
+
+$servername = "localhost";
+$username = "tavo";
+$password = "";
+$dbname = "myDB";
+
+// Create Connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// $conn->close();
+ $conn->close();
+
+?>
